a66a2d89272318b3e3a27fd1afff2dad3cd13f84
ifernisimo <47306095+ifernisimo@users.noreply.github.com>
2020-12-09 19:56:50 +0200

    auth - reg started

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
+const authRoutes = require('./routes/auth')
 const ordersRoutes = require('./routes/orders')
 const User = require('./models/user')
 
@@ -32,7 +33,6 @@ app.use(async (req,res,next)=>{
   } catch (e) {
     console.log(e)
   }
-  
 })
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -43,6 +43,7 @@ app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
 app.use('/orders', ordersRoutes)
+app.use('/auth', authRoutes)
 
 const PORT = process.env.PORT || 3000
 
