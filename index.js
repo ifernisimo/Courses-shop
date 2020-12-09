522bdaf6b31760b9a1fde4d2a6b184fa7814f1b1
ifernisimo <47306095+ifernisimo@users.noreply.github.com>
2020-12-09 14:40:11 +0200

    :

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
+const ordersRoutes = require('./routes/orders')
+const User = require('./models/user')
+
+const app = express()
+
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "hbs",
+  runtimeOptions: {
+    allowProtoPropertiesByDefault: true,
+    allowProtoMethodsByDefault: true,
+  },
+});
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(async (req,res,next)=>{
+  try {
+    const user = await User.findById('5fca13bb84fc2c0fac61c9c1')
+    req.user = user
+    next()
+  } catch (e) {
+    console.log(e)
+  }
+  
+})
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}))
+
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
+app.use('/card', cardRoutes)
+app.use('/orders', ordersRoutes)
+
+const PORT = process.env.PORT || 3000
+
+async function start() {
+  try {
+    const url = `mongodb+srv://makedasoul:00000000@cluster0.n00xm.mongodb.net/shop?retryWrites=true&w=majority`
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    })
+    const candidate = await User.findOne()
+    if (!candidate) {
+      const user = new User({
+        email: 'valera@yanchev.com',
+        name: 'Valera',
+        cart: {items: []}
+      })
+      await user.save()
+    }
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
+
+
