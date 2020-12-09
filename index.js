2a2a47aad0ae27941523453e69b1db4557fe97c4
ifernisimo <47306095+ifernisimo@users.noreply.github.com>
2020-12-09 21:10:27 +0200

    mock auth logout

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
 const authRoutes = require('./routes/auth')
 const ordersRoutes = require('./routes/orders')
-const User = require('./models/user')
+const session = require('express-session')
+
+const varMiddleware = require('./middleware/variables');
 
 const app = express()
 
@@ -25,18 +27,15 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-app.use(async (req,res,next)=>{
-  try {
-    const user = await User.findById('5fca13bb84fc2c0fac61c9c1')
-    req.user = user
-    next()
-  } catch (e) {
-    console.log(e)
-  }
-})
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
+app.use(session({
+  secret: 'some secret value',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(varMiddleware)
 
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
@@ -55,15 +54,15 @@ async function start() {
       useFindAndModify: false,
       useUnifiedTopology: true
     })
-    const candidate = await User.findOne()
-    if (!candidate) {
-      const user = new User({
-        email: 'valera@yanchev.com',
-        name: 'Valera',
-        cart: {items: []}
-      })
-      await user.save()
-    }
+    // const candidate = await User.findOne()
+    // if (!candidate) {
+    //   const user = new User({
+    //     email: 'valera@yanchev.com',
+    //     name: 'Valera',
+    //     cart: {items: []}
+    //   })
+    //   await user.save()
+    // }
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`)
     })
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,34 @@
 const {Router} = require('express');
 const router = Router();
+const User = require('../models/user')
 
 router.get('/login', async (req, res) => {
     res.render('auth/login', {
         title: 'Авторизация',
         isLogin: true
     })
+})
+
+router.post('/login', async (req, res) => {
+
+    const user = await User.findById('5fca13bb84fc2c0fac61c9c1')
+    req.session.user = user
+    req.session.isAuthenticated = true
+    req.session.save(err => {
+        if (err) {
+            throw err
+        }
+        res.redirect('/')
+    })
+   
+})
+
+
+router.get('/logout', async (req, res) => {
+    req.session.destroy(()=> {
+        res.redirect('/auth/login#login')
+    })
+    
 })
 
 module.exports = router 
