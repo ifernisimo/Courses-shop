f69879c534a8d7f4c471c3dbb6a5ae2ec72757db
ifernisimo <47306095+ifernisimo@users.noreply.github.com>
2020-12-23 14:52:28 +0200

    file upload

--- routes/profile.js
+++ routes/profile.js
@@ -1,36 +1,34 @@
-const { Router } = require("express");
-const auth = require("../middleware/auth");
-const router = Router();
+const {Router} = require('express')
+const auth = require('../middleware/auth')
+const User = require('../models/user')
+const router = Router()
 
-router.get("/", async (req, res) => {
-  res.render("profile", {
-    title: "Профиль",
+router.get('/', auth, async (req, res) => {
+  res.render('profile', {
+    title: 'Профиль',
     isProfile: true,
-    user: req.user.toObject(),
-  });
-});
+    user: req.user.toObject()
+  })
+})
 
-router.post("/", auth, async (req, res) => {
+router.post('/', auth, async (req, res) => {
   try {
-    const user = await User.findById(req.user._id);
+    const user = await User.findById(req.user._id)
+
     const toChange = {
-      name: req.body.name,
-    };
-
-    console.log(req.file)
-
-
-    if (req.file) {
-      toChange.avatarUrl = "";
+      name: req.body.name
     }
 
-    
-    Object.assign(user, toChange);
-    await user.save();
-    res.redirect("/profile");
-  } catch (e) {
-    console.log(e);
-  }
-});
+    if (req.file) {
+      toChange.avatarUrl = req.file.path
+    }
 
-module.exports = router;
+    Object.assign(user, toChange)
+    await user.save()
+    res.redirect('/profile')
+  } catch (e) {
+    console.log(e)
+  }
+})
+
+module.exports = router
